5a6e79feff8d3694b6fb1749a719f81d8fee7a52
Haider-ext <Zeeshan.Haider-ext@bshg.com>
2022-05-20 12:21:22 +0530

    first commit

---
+++ src/main/java/zeeshan/App.java
@@ -0,0 +1,11 @@
+package zeeshan;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		System.out.println("Hello World! My name is HIP Testing.");
+	}
+
+}
